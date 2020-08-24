883e101f54b28f658db61a48c52f8916cad1f007
r00sha <r00sha@users.noreply.github.com>
2020-08-24 10:51:58 +0700

    Menampilkan data penjualan

--- app/src/main/java/id/ac/umn/magang01/PenjualanActivity.java
+++ app/src/main/java/id/ac/umn/magang01/PenjualanActivity.java
@@ -2,14 +2,211 @@ package id.ac.umn.magang01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
 
 public class PenjualanActivity extends AppCompatActivity {
+
+    private String TAG = PenjualanActivity.class.getSimpleName();
+
+    private ProgressDialog prog;
+    private ListView lv;
+    EditText etSearch;
+    ImageView imgBack, imgRefresh, imgSearch;
+
+    ArrayList<PenjualanModel> penjualan = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_penjualan);
 
+        lv = findViewById(R.id.listView);
+        lv.setTextFilterEnabled(true);
+        etSearch = findViewById(R.id.inputSearch);
+        String cari = etSearch.getText().toString().trim();
+        new GetContacts().execute(cari);
+
+        imgBack = findViewById(R.id.imgBack);
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParent();
+                finish();
+            }
+        });
+
+        imgRefresh = findViewById(R.id.imgRefresh);
+        imgRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshData();
+            }
+        });
+
+        etSearch = findViewById(R.id.inputSearch);
+        etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                    penjualan.clear();
+                    String cari = etSearch.getText().toString().trim();
+                    new GetContacts().execute(cari);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        imgSearch = findViewById(R.id.imgSearch);
+        imgSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                penjualan.clear();
+                String cari = etSearch.getText().toString().trim();
+                new GetContacts().execute(cari);
+            }
+        });
+    }
+
+    private void refreshData() {
+        startActivity(new Intent(PenjualanActivity.this, PenjualanActivity.class));
+        finish();
+    }
+
+    private void  showProgressDialog(){
+        if(prog == null){
+            prog = new ProgressDialog(PenjualanActivity.this);
+            prog.setMessage("Fetching data...");
+            prog.setIndeterminate(false);
+            prog.setCancelable(false);
+        }
+        prog.show();
+    }
+
+    private void dismissProgressDialog() {
+        if (prog != null && prog.isShowing()) {
+            prog.dismiss();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        dismissProgressDialog();
+        super.onDestroy();
+    }
+
+    private class GetContacts extends AsyncTask<String, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showProgressDialog();
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            HttpHandler sh = new HttpHandler();
+
+            String url = Setting.API_Penjualan_Dagang;
+            String jsonStr = sh.makeServiceCall(url);
+
+            DecimalFormat pemisahRibuan = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+            DecimalFormatSymbols formatPemisah = new DecimalFormatSymbols();
+
+            formatPemisah.setCurrencySymbol("");
+            formatPemisah.setMonetaryDecimalSeparator(',');
+            formatPemisah.setGroupingSeparator('.');
+
+            pemisahRibuan.setDecimalFormatSymbols(formatPemisah);
+
+            if(jsonStr == null) Log.e(TAG, "JSON null");
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("data");
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String id = c.getString("CustCode");
+                        String name = c.getString("FullName");
+                        String yrmonth = c.getString("TahunBulan");
+                        int nilaiPenjualan = c.getInt("NilaiPenjualan");
+
+                        String nilai = pemisahRibuan.format(nilaiPenjualan);
+
+                        String search = strings[0];
+                        if(search.isEmpty()){
+                            penjualan.add(new PenjualanModel(id, name, yrmonth, pemisahRibuan.format(nilaiPenjualan).substring(0, nilai.length()-3)));
+                        }
+                        else if(name.contains(search.toUpperCase())) {
+                            penjualan.add(new PenjualanModel(id, name, yrmonth, pemisahRibuan.format(nilaiPenjualan).substring(0, nilai.length()-3)));
+                        }
+                    }
+
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (PenjualanActivity.this.isDestroyed()){
+                return;
+            }
+            dismissProgressDialog();
+
+            lv.setAdapter(new PenjualanAdapter(PenjualanActivity.this, R.layout.list_penjualan, penjualan));
+        }
     }
 }
